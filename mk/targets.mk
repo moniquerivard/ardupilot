acf73ba1d55525cd02e41b17837f8322586188eb
moniquerivard <50745611+moniquerivard@users.noreply.github.com>
2019-06-10 15:05:37 -0400

    Add files via upload

--- mk/targets.mk
+++ mk/targets.mk
@@ -1,4 +1,4 @@
-default: all
+default: help
 
 # convenient targets for our supported boards
 sitl: HAL_BOARD = HAL_BOARD_SITL
@@ -16,6 +16,7 @@ apm1: all
 apm1-1280: HAL_BOARD = HAL_BOARD_APM1
 apm1-1280: TOOLCHAIN = AVR
 apm1-1280: all
+apm1-1280: BOARD = mega
 
 apm2: HAL_BOARD = HAL_BOARD_APM2
 apm2: TOOLCHAIN = AVR
@@ -31,9 +32,9 @@ linux: HAL_BOARD = HAL_BOARD_LINUX
 linux: TOOLCHAIN = NATIVE
 linux: all
 
-erle: HAL_BOARD = HAL_BOARD_LINUX
-erle: TOOLCHAIN = BBONE
-erle: all
+erleboard: HAL_BOARD = HAL_BOARD_LINUX
+erleboard: TOOLCHAIN = BBONE
+erleboard: all
 
 zynq: HAL_BOARD = HAL_BOARD_LINUX
 zynq: TOOLCHAIN = ZYNQ
@@ -45,9 +46,17 @@ pxf: HAL_BOARD = HAL_BOARD_LINUX
 pxf: TOOLCHAIN = BBONE
 pxf: all
 
+bebop: HAL_BOARD = HAL_BOARD_LINUX
+bebop: TOOLCHAIN = BBONE
+bebop: all
+
 navio: HAL_BOARD = HAL_BOARD_LINUX
 navio: TOOLCHAIN = RPI
 navio: all
+
+raspilot: HAL_BOARD = HAL_BOARD_LINUX
+raspilot: TOOLCHAIN = RPI
+raspilot: all
 
 bbbmini: HAL_BOARD = HAL_BOARD_LINUX
 bbbmini: TOOLCHAIN = BBONE
@@ -72,7 +81,7 @@ empty: all
 
 # cope with copter and hil targets
 FRAMES = quad tri hexa y6 octa octa-quad heli single coax obc nologging
-BOARDS = apm1 apm2 apm2beta apm1-1280 px4 px4-v1 px4-v2 sitl flymaple linux vrbrain vrbrain-v40 vrbrain-v45 vrbrainv-50 vrbrain-v51 vrbrain-v52 vrubrain-v51 vrubrain-v52 vrhero-v10 erle pxf navio bbbmini
+BOARDS = apm1 apm2 apm2beta apm1-1280 px4 px4-v1 px4-v2 sitl flymaple linux vrbrain vrbrain-v40 vrbrain-v45 vrbrainv-50 vrbrain-v51 vrbrain-v52 vrubrain-v51 vrubrain-v52 vrhero-v10 erle pxf navio raspilot bbbmini
 
 define frame_template
 $(1)-$(2) : EXTRAFLAGS += "-DFRAME_CONFIG=$(shell echo $(2) | tr a-z A-Z | sed s/-/_/g)_FRAME "
@@ -82,13 +91,21 @@ $(1)-$(2)-debug : $(1)-$(2)
 $(1)-$(2)-hilsensors : $(1)-$(2)
 $(1)-$(2)-upload : $(1)-$(2)
 $(1)-$(2)-upload : $(1)-upload
+endef
+
+define board_template
 $(1)-hil : $(1)
 $(1)-debug : $(1)
 $(1)-hilsensors : $(1)
 endef
 
+USED_BOARDS := $(foreach board,$(BOARDS), $(findstring $(board), $(MAKECMDGOALS)))
+USED_FRAMES := $(foreach frame,$(FRAMES), $(findstring $(frame), $(MAKECMDGOALS)))
+#$(warning $(USED_BOARDS))
+#$(warning $(USED_FRAMES))
 # generate targets of the form BOARD-FRAME and BOARD-FRAME-HIL
-$(foreach board,$(BOARDS),$(foreach frame,$(FRAMES),$(eval $(call frame_template,$(board),$(frame)))))
+$(foreach board,$(USED_BOARDS),$(eval $(call board_template,$(board))))
+$(foreach board,$(USED_BOARDS),$(foreach frame,$(USED_FRAMES),$(eval $(call frame_template,$(board),$(frame)))))
 
 apm2beta: EXTRAFLAGS += "-DAPM2_BETA_HARDWARE "
 apm2beta: apm2
@@ -104,3 +121,5 @@ etags:
 
 clean:
 	@rm -fr $(BUILDROOT)
+
+include $(MK_DIR)/modules.mk
