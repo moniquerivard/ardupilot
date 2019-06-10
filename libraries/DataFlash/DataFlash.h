67d9c52b3f7f40ce6aef116d8484502da7a40190
U-RMC-CMR\rivard <rivard@pcMEC95B7F6.rmc-cmr.rmc.ca>
2019-06-10 14:36:01 -0400

    back dataflash

--- libraries/DataFlash/DataFlash.h
+++ libraries/DataFlash/DataFlash.h
@@ -44,10 +44,11 @@ class DataFlash_Class
 public:
     FUNCTOR_TYPEDEF(print_mode_fn, void, AP_HAL::BetterStream*, uint8_t);
     FUNCTOR_TYPEDEF(vehicle_startup_message_Log_Writer, void);
-    DataFlash_Class(const prog_char_t* firmware_string) :
-        _startup_messagewriter(DFMessageWriter_DFLogStart(*this, firmware_string)),
-        _vehicle_messages(NULL)
-     { }
+    DataFlash_Class(const char *firmware_string) :
+        _firmware_string(firmware_string)
+        {
+            AP_Param::setup_object_defaults(this, var_info);
+        }
 
     void set_mission(const AP_Mission *mission);
 
