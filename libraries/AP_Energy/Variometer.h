265339d30b7f81058b67426a70acbe2d50c29a6a
U-RMC-CMR\rivard <rivard@pcMEC95B7F6.rmc-cmr.rmc.ca>
2019-07-29 12:38:25 -0400

    compiling dsoar library

--- libraries/AP_Energy/Variometer.h
+++ libraries/AP_Energy/Variometer.h
@@ -1,3 +1,5 @@
+#ifndef __VARIOMETER_H__
+#define __VARIOMETER_H__
 
 #include <AP_AHRS/AP_AHRS.h>
 #include <AP_Param/AP_Param.h>
@@ -7,11 +9,8 @@ class Variometer {
        
 public:
     //constructor
-    Variometer(const AP_AHRS& ahrs):
+    Variometer(const AP_AHRS& ahrs) :
         _ahrs(ahrs)
-    {
-        AP_Param::setup_object_defaults(this, var_info);
-    }
 
     float total_E;
     float prev_energy;
@@ -30,4 +29,10 @@ public:
     // Airspeed Sensors
     AP_Airspeed airspeed;
 
+private:
+    const AP_AHRS& _ahrs;
+
+
 };
+
+#endif //__VARIOMETER_H__
