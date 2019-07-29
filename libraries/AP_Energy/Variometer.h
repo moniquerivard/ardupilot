be366726db3660597ca8a25fbab52b081078f277
U-RMC-CMR\rivard <rivard@pcMEC95B7F6.rmc-cmr.rmc.ca>
2019-07-29 12:42:25 -0400

    compiling dsoar library

--- libraries/AP_Energy/Variometer.h
+++ libraries/AP_Energy/Variometer.h
@@ -9,8 +9,8 @@ class Variometer {
        
 public:
     //constructor
-    Variometer(const AP_AHRS& ahrs) :
-        _ahrs(ahrs)
+    Variometer(const AP_AHRS& ahrs):
+        _ahrs(ahrs);
 
     float total_E;
     float prev_energy;
